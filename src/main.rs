f3861ec71a9ca5bdc09d0501b63bedcde712e313
Aaron Weiss <awe@pdgn.co>
2018-03-01 23:42:56 +0100

    Built up a first-cut at meep without syntax highlighting.

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@
 //! You should have received a copy of the GNU Affero General Public License
 //! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#![feature(plugin)]
+#![feature(custom_derive, plugin)]
 #![plugin(rocket_codegen)]
 
 #[macro_use]
@@ -28,13 +28,23 @@ extern crate diesel;
 extern crate dotenv;
 #[macro_use]
 extern crate dotenv_codegen;
+extern crate failure;
 extern crate r2d2_diesel;
 extern crate r2d2;
+extern crate rand;
 extern crate rocket;
 extern crate syntect;
 
+use std::borrow::Cow;
+use std::ops::Deref;
+
+use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use r2d2_diesel::ConnectionManager;
+use rand::Rng;
+use rocket::{Data, Outcome, Request, State};
+use rocket::http::{RawStr, Status};
+use rocket::request::{self, FromParam, FromRequest};
 
 //    __             ___                 __
 //   /\ \          /'___\ __          __/\ \__  __
@@ -44,11 +54,50 @@ use r2d2_diesel::ConnectionManager;
 //  \ \___,_\ \____\\ \_\  \ \_\ \_\ \_\ \_\ \__\\ \_\ \____/\ \_\ \_\/\____/
 //   \/__,_ /\/____/ \/_/   \/_/\/_/\/_/\/_/\/__/ \/_/\/___/  \/_/\/_/\/___/
 
+pub static DATABASE_URL: &'static str = dotenv!("DATABASE_URL");
 
-type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+pub type Result<T> = std::result::Result<T, failure::Error>;
+pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
 pub struct DbConn(pub r2d2::PooledConnection<ConnectionManager<SqliteConnection>>);
 
-static DATABASE_URL: &'static str = dotenv!("DATABASE_URL");
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<DbConn, ()> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(DbConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+impl Deref for DbConn {
+    type Target = SqliteConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct PasteId<'a>(Cow<'a, str>);
+
+impl<'a> FromParam<'a> for PasteId<'a> {
+    type Error = &'a RawStr;
+
+    fn from_param(param: &'a RawStr) -> std::result::Result<PasteId<'a>, &'a RawStr> {
+        let valid = param.chars().all(|c| {
+            (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
+        });
+
+        if valid {
+            Ok(PasteId(Cow::Borrowed(param)))
+        } else {
+            Err(param)
+        }
+    }
+}
 
 //                 ___
 //   __          /'___\
@@ -61,11 +110,11 @@ static DATABASE_URL: &'static str = dotenv!("DATABASE_URL");
 fn main() {
     rocket::ignite()
         .manage(init_pool())
-        .mount("/", routes![index])
+        .mount("/", routes![index, paste, view])
         .launch();
 }
 
-fn init_pool() -> Pool {
+pub fn init_pool() -> Pool {
     let manager = ConnectionManager::<SqliteConnection>::new(DATABASE_URL);
     r2d2::Pool::new(manager).expect("failed to create db pool")
 }
@@ -80,6 +129,89 @@ fn init_pool() -> Pool {
 
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+pub fn index() -> &'static str {
+    r#"meep(1)                              MEEP                               meep(1)
+
+     dMMMMMMMMb dMMMMMP dMMMMMP dMMMMb
+    dMP"dMP"dMPdMP     dMP     dMP.dMP
+   dMP dMP dMPdMMMP   dMMMP   dMMMMP"
+  dMP dMP dMPdMP     dMP     dMP
+ dMP dMP dMPdMMMMMP dMMMMMP dMP
+
+SYNOPSIS
+    <command> | curl --data-binary "@-" https://commie.club/meep
+
+DESCRIPTION
+    add ?<lang> to resulting url for line numbers and syntax highlighting
+
+EXAMPLES
+    [to do]
+
+SEE ALSO
+    http://github.com/aatxe/meep
+"#
+}
+
+#[post("/", data = "<data>")]
+pub fn paste(conn: DbConn, data: Data) -> Result<String> {
+    use models::*;
+    use schema::*;
+
+    let mut buf = Vec::new();
+    let _ = data.stream_to(&mut buf)?;
+    let str_data = String::from_utf8(buf)?;
+
+    let id = rand::thread_rng().gen_ascii_chars().take(4).collect();
+    let url = format!("https://commie.club/meep/{}", &id);
+
+    let paste = Paste {
+        id: id,
+        data: str_data,
+    };
+
+    diesel::insert_into(pastes::table)
+        .values(&paste)
+        .execute(&*conn)?;
+
+    Ok(url)
+}
+
+#[get("/<pid>")]
+pub fn view(conn: DbConn, pid: PasteId) -> Result<String> {
+    use models::*;
+    use schema::pastes::dsl::*;
+
+    let paste = pastes.find(pid.0)
+        .first::<Paste>(&*conn)?;
+
+    Ok(paste.data)
+}
+
+
+//                        __          ___             
+//                       /\ \        /\_ \            
+//    ___ ___     ___    \_\ \     __\//\ \     ____  
+//  /' __` __`\  / __`\  /'_` \  /'__`\\ \ \   /',__\ 
+//  /\ \/\ \/\ \/\ \L\ \/\ \L\ \/\  __/ \_\ \_/\__, `\
+//  \ \_\ \_\ \_\ \____/\ \___,_\ \____\/\____\/\____/
+//   \/_/\/_/\/_/\/___/  \/__,_ /\/____/\/____/\/___/ 
+    
+pub mod models {
+    use super::schema::pastes;
+
+    #[derive(Insertable, Queryable)]
+    #[table_name="pastes"]
+    pub struct Paste {
+        pub id: String,
+        pub data: String,
+    }
+}
+    
+pub mod schema {
+    table! {
+        pastes (id) {
+            id -> Text,
+            data -> Text,
+        }
+    }
 }
